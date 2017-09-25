2a05d655075058d56e258f0d021e1fcd1250480a
Jarnotouw16 <d228459@edu.rocwb.nl>
2017-09-25 12:17:24 +0200

    added salespage

---
+++ Project_folder/Barroc-IT/public/sales.php
@@ -0,0 +1,29 @@
+<?php
+require ('../app/database.php');
+require ('head.php');
+?>
+    <header>
+        <h3>sales department<h3>
+    </header>
+    <div class="container">
+        <div class="row">
+            <div>
+                <img src="../img/icon-sales-add.png" alt="">
+                <li><a href="#">add customer</a></li>
+            </div>
+            
+            <div>
+                <img src="" alt="">
+                <li><a href="#">edit customer</a></li>
+            </div>
+            
+            <div>
+                <img src="" alt="">
+                <li><a href="#">delete customer</a></li>
+            </div>
+            
+        </div>
+    </div>
+<?php
+require ('foot.php');
+?>
