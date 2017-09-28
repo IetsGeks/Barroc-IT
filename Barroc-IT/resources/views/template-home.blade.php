a002699fc1bde1f1e702759bc814e297d722f41d
Unknown <santidudok@hotmail.com>
2017-09-28 23:16:51 +0200

    Template-home update

---
+++ Barroc-IT/resources/views/template-home.blade.php
@@ -0,0 +1,21 @@
+
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <title>Template Home</title>
+    </head>
+    <body>
+        <header>
+            <ul class="nav-bar">
+                <li><a href="{{url('/projects')}}">projects</a></li>
+                <li>Projects</li>
+            </ul>
+        </header>
+        <div class="banner">
+
+        </div>
+        <div class="row">
+
+        </div>
+    </body>
+    </html>
