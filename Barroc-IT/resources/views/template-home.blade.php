80ae088744a19f298ec471abbd07c750c936525e
Unknown <santidudok@hotmail.com>
2017-09-29 12:15:14 +0200

    Template home 29-09

--- Barroc-IT/resources/views/template-home.blade.php
+++ Barroc-IT/resources/views/template-home.blade.php
@@ -1,21 +1,59 @@
+<?php
+    require ("../../public/head.php")
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Template Home</title>
+    <link rel="stylesheet" href="../../public/css/template-home.css" />
+</head>
+<body>
+<header class="header-sales">
+    <div class="container">
+        <div class="row-spaced">
+            <div class="nav">
+                <ul>
+                    <li><a href="{{url('/sales')}}">Home</a></li>
+                    <li><a href="{{url('/projects')}}">Projects</a></li>
+                    <li><a href="{{url('/customers')}}">customers</a></li>
+                </ul>
 
-    <!doctype html>
-    <html lang="en">
-    <head>
-        <title>Template Home</title>
-    </head>
-    <body>
-        <header>
-            <ul class="nav-bar">
-                <li><a href="{{url('/projects')}}">projects</a></li>
-                <li>Projects</li>
-            </ul>
-        </header>
-        <div class="banner">
+            </div>
+            <div class="account">
+                <h3>Sales</h3>
+            </div>
+        </div>
+        <div class="row-spaced">
+
+
+    <!--            --><?php
+    //                if (isset($_SESSION{'sales'}))
+    //                {
+    //                    echo '<h3>' . $username . '</h3>';
+    //                }else if (isset($_SESSION['finance']))
+    //                {
+    //                    echo '<h3>' . $username . '</h3>';
+    //                }else if (isset($_SESSION['development']))
+    //                {
+    //                    echo '<h3>' . $username . '</h3>';
+    //                }else if (isset($_SESSION['administration']))
+    //                {
+    //                    echo '<h3>' . $username . '</h3>';
+    //                }
+    //            ?>
 
         </div>
-        <div class="row">
+    </div>
+</header>
+    <div class="banner">
+        <div class="container">
 
         </div>
-    </body>
-    </html>
+    </div>
+    <div class="row">
+        <div class="container">
+
+        </div>
+    </div>
+</body>
+</html>
