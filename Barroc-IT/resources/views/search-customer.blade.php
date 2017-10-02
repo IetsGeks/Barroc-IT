3c0f56908826f476555649d0a8de55a881280f9e
Bm4n <D230966@edu.rocwb.nl>
2017-10-02 12:05:20 +0200

    help functie searchC
    + the help function for searchC (incompleet)

--- Barroc-IT/resources/views/search-customer.blade.php
+++ Barroc-IT/resources/views/search-customer.blade.php
@@ -10,6 +10,8 @@
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 </head>
 <body>
     <header class="header-sales">
@@ -26,6 +28,11 @@
             {{csrf_field()}}
             <input class="searchB form-control" type="text" id="searchC" name="searchC">
             <input class="searchS btn btn-primary" type="submit" value="Search">
+            <button type="button" onclick="switchH()">
+                <div class="helpf">
+                    <p>?</p>
+                </div>
+            </button>
         </form>
         <table class="table table-hover table-sm">
             <thead>
@@ -65,10 +72,49 @@
             </tr>
             </tbody>
         </table>
-        <div class="error" id="hidden">
+        <div class="error" id="hiddenER">
             <div><p>Oops!</p><p>Have you tryed to turn it off and on again?</p></div>
         </div>
+        <div class="helptxt" id="hidden">
+            <div class="helptxtnl">
+                <p><b>Inloggen</b></p>
+                <p>
+                    Wanneer je de website opzoekt vindt je het log-in scherm.
+                    Hier vul je de inlog gegevens van jouw afdeling in die je
+                    Zou moeten hebben gehad van het bedrijf. Als je dit niet
+                    hebt gehad neem dan contact op met je afdelings-manager.
+                </p>
+            </div>
+            <div class="helptxten">
+                <p><b>Log-in</b></p>
+                <p>
+                    When you search for the website you will find a log-in screen.
+                    You need to put the log-in data of your department in the
+                    input fields. If you don't have that, please contact your
+                    departement-manager.
+                </p>
+            </div>
+        </div>
     </div>
+    <script type="text/javascript">
+
+        function switchH(){
+            if (document.getElementsByClassName("helptxt")[0].id == "hidden") {
+                HtS();
+            }else{
+                StH();
+            }
+        }
+
+        function StH(){
+            $('div#shown').attr("id", "hidden");
+        }
+
+        function HtS(){
+            $('div#hidden').attr("id", "shown");
+        }
+
+    </script>
 </body>
 </html>
 
