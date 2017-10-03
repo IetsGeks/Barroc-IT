96abd5beb6c53e39f61acaed58e0ad7aebc03d44
Unknown <santidudok@hotmail.com>
2017-10-03 12:14:55 +0200

    home template bijna af
    alleen header verbeteren

--- Barroc-IT/resources/views/template-home.blade.php
+++ Barroc-IT/resources/views/template-home.blade.php
@@ -1,6 +1,13 @@
-<?php
-//    require ("../../public/head.php")
-?>
+
+{{--{{$salesHome1 = 'Sales department'}}--}}
+{{--{{$salesHome2 = 'Welcome to the Sales department control panel. Here we manage all the projects send in by our costumers.'}}--}}
+{{--{{$financeHome1 = 'Finance department'}}--}}
+{{--{{$financeHome2 = 'Welcome to the Finance department control panel. Here we manage all the expenses of the costumers and our company'}}--}}
+{{--{{$adminHome1 = 'Admin panel'}}--}}
+{{--{{$adminHome2 = 'Welcome to the admin panel. Here we control the costumers who passed their credit limit.'}}--}}
+{{--{{$developmentHome1 = 'Development department'}}--}}
+{{--{{$developmentHome2 = 'Welcome to the Development department. Here we can see all the projects wich can be worked on and wich cant be worked on'}}--}}
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -26,39 +33,47 @@
             <li><a href="{{url('/projects')}}">Projects</a></li>
             <li><a href="{{url('/customers')}}">customers</a></li>
         </ul>
-
         <div class="department1">
             <h3>Sales</h3>
+
+
+            @if (isset($_SESSION{'sales'}))
+                 {{'<h3>' . $username . '</h3>'}}
+            @elseif (isset($_SESSION['finance']))
+                 {{'<h3>' . $username . '</h3>'}}
+            @elseif (isset($_SESSION['development']))
+                 {{'<h3>' . $username . '</h3>'}}
+            @elseif (isset($_SESSION['administration']))
+                 {{'<h3>' . $username . '</h3>'}}
+            @endif
+
         </div>
     </div>
 
 
 </header>
-
-
-<div class="check-account">
-    <?php
-        if (isset($_SESSION{'sales'}))
-        {
-            echo '<h3>' . $username . '</h3>';
-        }else if (isset($_SESSION['finance']))
-        {
-            echo '<h3>' . $username . '</h3>';
-        }else if (isset($_SESSION['development']))
-        {
-            echo '<h3>' . $username . '</h3>';
-        }else if (isset($_SESSION['administration']))
-        {
-            echo '<h3>' . $username . '</h3>';
-        }
-    ?>
-</div>
-
     <div class="banner">
         <div class="container">
             <h3>Sales department</h3>
+            <span>Welcome to the Sales department control panel. Here we manage all the projects send in by our costumers.</span>
 
-                <p>Welcome to the Sales department control panel. Here we manage all the projects send in by our costumers.</p>
+                @if (isset($_SESSION{'sales'}))
+                    {{'<h3>' . $salesHome1 . '</h3>'}}
+                    {{ '<span>' . $salesHome2 . '</span>'}}
+                @elseif (isset($_SESSION['finance']))
+
+                     {{'<h3>' . $financeHome1 . '</h3>'}}
+                     {{'<span>' . $financeHome2 . '</span>'}}
+                @elseif (isset($_SESSION['development']))
+
+                     {{'<h3>' . $developmentHome1 . '</h3>'}}
+                     {{ '<span>' . $developmentHome2 . '</span>'}}
+                @elseif (isset($_SESSION['administration']))
+
+                     {{ '<h3>' . $adminHome1 . '</h3>'}}
+                     {{ '<span>' . $adminHome2 . '</span>'}}
+                @endif
+
 
         </div>
     </div>
