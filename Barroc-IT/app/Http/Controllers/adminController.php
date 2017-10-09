c03eb8ccd76ff8310ae09994ef3ea798cb7d2297
Jarnotouw16 <d228459@edu.rocwb.nl>
2017-10-09 09:17:32 +0200

    website
    *adminpanel

--- Barroc-IT/app/Http/Controllers/adminController.php
+++ Barroc-IT/app/Http/Controllers/adminController.php
@@ -12,8 +12,9 @@ class adminController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
+    {   //$users = \App\Customer::all();
         return view('admin');
+           // ->with('users', $users);
     }
 
     /**
